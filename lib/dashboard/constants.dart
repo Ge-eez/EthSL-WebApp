b8fc859e1c2bf56417763559882e75d3027d8f03
MetiLe <se.meti.leggese@gmail.com>
2023-06-02 07:08:10 +0300

    dashboard

---
+++ lib/dashboard/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF2697FF);
+const secondaryColor = Colors.white;
+const bgColor = Color.fromRGBO(237, 231, 246, 1);
+
+const defaultPadding = 16.0;
---
+++ lib/dashboard/main.dart
@@ -0,0 +1,34 @@
+import 'package:blink/dashboard/constants.dart';
+import 'package:blink/dashboard/controllers/MenuAppController.dart';
+import 'package:blink/dashboard/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter blink/dashboard Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme).apply(bodyColor: Color(0xff33357D)),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuAppController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
+    );
+  }
+}
